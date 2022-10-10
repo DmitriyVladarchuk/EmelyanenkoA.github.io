6eef482a978a59f9930624c29f482405e7089414
EmelyanenkoA <112863032+EmelyanenkoA@users.noreply.github.com>
2022-10-10 13:59:45 +0300

    Update Script.js

--- Lesson5/Script.js
+++ Lesson5/Script.js
@@ -1,24 +1,27 @@
 function task1 (){
     var n1, n2, res;
     n1 = document.getElementById('field1').value;
-    n1 = parseInt(n1);
-
     n2 = document.getElementById('field2').value;
-    n2 = parseInt(n2);
+    var regex = /^[1-9]\d+$/;
 
-    if (isNaN(n1) || isNaN(n2))
+    if (!regex.test(n1) || !regex.test(n2))
     {
-        alert("Неккоректный запрос, проверьте чтобы оба значения были числами");
+        alert("Неккоректный запрос, проверьте чтобы оба значения были положитенльными числами");
         return false;
     }
+    
     alert("Ваш запрос выполняется");
+    n1 = parseInt(n1);
+    n2 = parseInt(n2);
     res = n1 * n2;
     document.getElementById('output').innerHTML = "Итоговая цена: " + res;
 
 }
 
+
+    
 window.addEventListener('DOMContentLoaded', function (event) {
     console.log("DOM fully loaded and parsed");
-    let a= document.getElementById("click");
-    a.addEventListener("task1");
+    let a = document.getElementById("click");
+    a.addEventListener("Click",task1);
 });
